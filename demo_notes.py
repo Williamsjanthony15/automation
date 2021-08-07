574a3b3b29433a9de0693672017564bb64afe195
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-08-07 16:23:16 -0500

    Para, email, phone working and appending to paragraph.txt file

--- demo_notes.py
+++ demo_notes.py
@@ -10,76 +10,85 @@ fake = Faker('en_US')
 # print(fake)
 
 # This tells me all of the methods i can fake with this object
-print(dir(fake))
+# print(dir(fake))
 
 # Random Names it will generate upon running the script
-print(fake.name())
+# print(fake.name())
 
 # Random City
-print(fake.city())
+# print(fake.city())
 
 # Random Credit Card
-print(fake.credit_card())
+# print(fake.credit_card())
 
 # Giving 100 Names
 # If dont want duplicates put names in set
 # First name female
-for i in range(100):
-    print(fake.first_name_female())
+# for i in range(100):
+    # print(fake.first_name_female())
 
 
 
 # Returns a string of word(s)
-print(fake.word())
+# print(fake.word())
 # Returns a list of 3 words Can have as many words as you want by calling a number.
-print(fake.words())
+# print(fake.words())
 #returns a string of words
-print(fake.sentance())
+# print(fake.sentance())
 # Returns a list of sentenaces
-print(fake.sentenaces())
+# print(fake.sentenaces())
 #Returns a paragraph
-print(fake.paragraph())
+# print(fake.paragraph())
 #return paragraphs
-print(fake.paragraphs())
+# print(fake.paragraphs())
 #return email Gives you multiple emails
-for i in range(10):
-    print(fake.email())
+# for i in range(10):
+    # print(fake.email())
 #return phone number
-print(fake.phone_number())
+# print(fake.phone_number())
 
 
 # making a file with paragraphs, emails, and phone numbers
-content = ''
+para = ''
+for i in range(10):
+    para = fake.sentence(5) + ' '
 
-for i in range(100):
-    content += fake.paragraph(2)
-    content += fake.email()
-    content += fake.paragraph(2)
-    content += fake.phone_number()
-    content += fake.paragraph()
-    # each set of data to be own line
-    content += '/n'
-print(content)
+email = ''
+for i in range(10):
+    email = fake.email('gmail.com')
+
+phone = ''
+for i in range(10):
+    phone = fake.phone_number()
+
+content = f'{para} {email} {phone} \n'
+
+# for i in range():
+#     content
+#     # each set of data to be own line
+#     content += '\n'
+# print(content)
+
 
 #Createing a write file with content in it and put it in the assets folder
-with open('assets/notes.txt', 'w+') as f:
-    f.write(contents)
+with open('assets/paragraph.txt', 'w+') as f:
+    f.write(content)
 
 #Reading the written file with the content in it
-with open('assets/notes.txt', 'w+') as f:
-    f.read(contents)
+# with open('assets/paragraph.txt', 'w+') as f:
+#     f.read()
 
 #poetry a While reading the file, If it meets the phone numbers description Move it to Phone_Number.txt file
 # If it meets the email description move it to the Email.txt file
 
 
 # copying the created file from assets into demo folder
-import shutil
+# import shutil
 
-shutil.copy('assets/notes.txt', '.')
+# shutil.copy('assets/notes.txt', '.')
 
 #moving created file from assets into demo folder
 
-shutil.move('assets/notes.txt', '.')
+# shutil.move('assets/notes.txt', '.')
 
 
